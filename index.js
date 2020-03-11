4169835e7c57f0384656ac2c4276bba5394fed6a
George Lugo <stg77713@gmail.com>
2020-03-11 18:16:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicianArray, instrumentsArray){
 }
 
 function johnLennonFacts(johnArray){
-  const facts = [
+  var facts = [
     "He was the last Beatle to learn to drive",
     "He was never a vegetarian",
     "He was a choir boy and boy scout",
