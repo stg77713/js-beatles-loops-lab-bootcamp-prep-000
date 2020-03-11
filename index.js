138fdae48af4c3be1f9cef8a2ad1634ee500b4bb
George Lugo <stg77713@gmail.com>
2020-03-11 18:04:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicianArray, instrumentsArray){
+  var allPlayers = [];
+  
+  for (let i = 0; i < musicianArray.length; i++){
+    allPlayers.push(musicianArray[i] + " plays " + instrumentsArray[i]);
+  }
+  return allPlayers;
+}
